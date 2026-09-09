94425bdb5d821b2606f9764def6ce8d359e1ffd8
dmercadante1 <mercadante.daniel@gmail.com>
2026-09-09 14:31:01 -0400

    15C batch 2 point enemy registry to production atlas

--- game/assets/characters.js
+++ game/assets/characters.js
@@ -1,7 +1,5 @@
 window.MWCAM_ASSETS=window.MWCAM_ASSETS||{};
 Object.assign(window.MWCAM_ASSETS,{
   hero:'./assets/production/characters/hero_walk.png?v=15b',
-  skeleton:'./assets/horror/skeleton_sheet.svg?v=04',
-  ghoul:'./assets/horror/ghoul_sheet.svg?v=04',
-  knight:'./assets/horror/knight_sheet.svg?v=04'
+  enemyAtlas:'./assets/production/characters/enemy_atlas.png?v=15c'
 });
